8b4af68c4eadaf17988d31e69432cba6125e841a
luanrafael <luan-g8@hotmail.com>
2013-09-16 23:42:56 -0300

    Endereço

---
+++ ecommerce/src/main/java/org/fatec/les/model/cliente/Endereco.java
@@ -0,0 +1,64 @@
+package org.fatec.les.model.cliente;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Endereco {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String rua;
+	private Integer numero;
+	private String bairro;
+	private String cidade;
+	private String estado;
+	private String pais;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getRua() {
+		return rua;
+	}
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+	public Integer getNumero() {
+		return numero;
+	}
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public String getPais() {
+		return pais;
+	}
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+	
+	
+}
