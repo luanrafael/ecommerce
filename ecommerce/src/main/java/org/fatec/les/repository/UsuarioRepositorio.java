d888ba513aa2741b7c6fac8a6d40a5ff70075f71
lucascampos <lucasg_campos@hotmail.com>
2013-09-19 21:00:13 -0300

    Modelo do Repositorio não implementado

---
+++ ecommerce/src/main/java/org/fatec/les/repository/UsuarioRepositorio.java
@@ -0,0 +1,31 @@
+package org.fatec.les.repository;
+
+import java.util.List;
+
+import org.fatec.les.model.usuario.Usuario;
+
+public class UsuarioRepositorio {
+
+	
+	public void inserirUSuario(Usuario usuario) {
+
+	}
+	
+	public void deletar(Usuario usuario) {
+		
+	}
+	
+	public Usuario buscar(Long id) {
+		return null;
+		
+	}
+	
+	public void editar(Usuario usuario) {
+		
+	}
+	
+	public List<Usuario> listarTodos() {
+		return null;
+		
+	}
+}
