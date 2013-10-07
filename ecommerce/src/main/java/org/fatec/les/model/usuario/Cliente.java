43f25dd577f1e9ea2c6667897ea2d1f5907e90e7
luanrafael <luan-g8@hotmail.com>
2013-10-07 08:42:09 -0300

    Repositorio Cliente

--- ecommerce/src/main/java/org/fatec/les/model/usuario/Cliente.java
+++ ecommerce/src/main/java/org/fatec/les/model/usuario/Cliente.java
@@ -1,9 +1,14 @@
 package org.fatec.les.model.usuario;
 
+import java.util.Date;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 
 @Entity
@@ -12,9 +17,81 @@ public class Cliente {
 	@Id
 	@GeneratedValue
 	private Long id;
-	private String name;
+	private String nome;
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEmail_alternativo() {
+		return email_alternativo;
+	}
+
+	public void setEmail_alternativo(String email_alternativo) {
+		this.email_alternativo = email_alternativo;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getCelular() {
+		return celular;
+	}
+
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+
+	public Date getData_nascimento() {
+		return data_nascimento;
+	}
+
+	public void setData_nascimento(Date data_nascimento) {
+		this.data_nascimento = data_nascimento;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	private String email;
+	private String email_alternativo;
+	private String cpf;
+	private String telefone;
+	private String celular;
 	
-	@OneToOne
+	@Temporal(TemporalType.DATE)
+	private Date data_nascimento;
+	
+	private String senha;
+	
+	
+	
+	@OneToOne(cascade=CascadeType.ALL)
 	private Endereco endereco;
 	
 	public void setEndereco(Endereco endereco){
@@ -34,11 +111,11 @@ public class Cliente {
 	}
 
 	public String getName() {
-		return name;
+		return nome;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
 	
 	
