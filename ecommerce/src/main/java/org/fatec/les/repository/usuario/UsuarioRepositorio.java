501541bd8eac9dd06694775dda12e2cf870e0f3e
lucas <lucasg_campos@hotmail.com>
2013-11-01 22:25:11 -0200

    Repositório

--- ecommerce/src/main/java/org/fatec/les/repository/usuario/UsuarioRepositorio.java
+++ ecommerce/src/main/java/org/fatec/les/repository/usuario/UsuarioRepositorio.java
@@ -2,30 +2,48 @@ package org.fatec.les.repository.usuario;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
 import org.fatec.les.model.usuario.Usuario;
 
-public class UsuarioRepositorio {
 
+public class UsuarioRepositorio {
+	
+	private EntityManager em;
+
+	public UsuarioRepositorio() {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("ecommerce");
+		em = emf.createEntityManager();
+	}
 	
 	public void inserirUSuario(Usuario usuario) {
-
+		em.getTransaction().begin();
+		if (usuario.getId() == null) {
+			em.persist(usuario);
+		} else {
+			em.merge(usuario);
+		}
+		em.getTransaction().commit();
 	}
 	
 	public void deletar(Usuario usuario) {
-		
+		em.getTransaction().begin();
+		em.remove(usuario);
+		em.getTransaction().commit();
 	}
 	
-	public Usuario buscar(Long id) {
-		return null;
-		
-	}
-	
-	public void editar(Usuario usuario) {
-		
+	public Usuario buscarUsuario(Long id) {
+		String jpql = "SELECT c FROM Usuario c where id = " + id.toString();
+		TypedQuery<Usuario> usuario = em.createQuery(jpql, Usuario.class);
+		return usuario.getSingleResult();
 	}
 	
 	public List<Usuario> listarTodos() {
-		return null;
-		
+		String jpql = "SELECT c FROM Usuario c";
+		TypedQuery<Usuario> usuarios = em.createQuery(jpql, Usuario.class);
+		return usuarios.getResultList();
 	}
 }
