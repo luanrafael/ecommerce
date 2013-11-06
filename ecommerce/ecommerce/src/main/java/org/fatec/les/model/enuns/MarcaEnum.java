722329ae1ecaddf1de2e6ab1830ad88c3d2b2a1d
root <root@rogerio.(none)>
2013-11-06 08:58:06 -0200

    Alterado a estrutura de PACKAGE, inserida a classe BaseRepositorio (alterando as classes de repositorio para extender da BaseRespositorio), juntamente com pequenas alterações na classe UsuarioService

---
+++ ecommerce/ecommerce/src/main/java/org/fatec/les/model/enuns/MarcaEnum.java
@@ -0,0 +1,10 @@
+package org.fatec.les.model.enuns;
+
+public enum MarcaEnum {
+	
+	//TODO: se necessário, inserir mais marcas.
+	NIKE,
+	ADDIDAS,
+	UMBRO,
+	PULMA;
+	}
---
+++ ecommerce/ecommerce/src/main/java/org/fatec/les/model/enuns/TimeEnum.java
@@ -0,0 +1,11 @@
+package org.fatec.les.model.enuns;
+
+public enum TimeEnum {
+
+	//TODO: inserir todos os times da série A 
+	CORINTHIANS,
+	PALMEIRAS,
+	SÃO_PAULO,
+	SANTOS;
+	
+}
