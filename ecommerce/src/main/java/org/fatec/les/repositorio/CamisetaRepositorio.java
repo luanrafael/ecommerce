07195ecaf57ed07f30e9e04a369e5f2c1e202f5b
lucascampos <lucasg_campos@hotmail.com>
2013-11-15 20:10:13 -0200

    Menu lateral

--- ecommerce/src/main/java/org/fatec/les/repositorio/CamisetaRepositorio.java
+++
@@ -1,7 +0,0 @@
-package org.fatec.les.repositorio;
-
-import org.fatec.les.model.entity.CamisetaEntity;
-
-public class CamisetaRepositorio extends BaseRepositorio<CamisetaEntity> {
-
-}
--- ecommerce/src/main/java/org/fatec/les/repositorio/PedidoRepositorio.java
+++
@@ -1,8 +0,0 @@
-package org.fatec.les.repositorio;
-
-import org.fatec.les.model.entity.PedidoEntity;
-
-
-public class PedidoRepositorio extends BaseRepositorio<PedidoEntity> {
-
-}
