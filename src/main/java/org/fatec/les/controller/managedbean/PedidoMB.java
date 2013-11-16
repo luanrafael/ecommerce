67792ef4c1bb90d2b360f9f6ce98d004f4800702
lucascampos <lucasg_campos@hotmail.com>
2013-11-15 22:20:47 -0200

    PedidoMB, FornecedorMB

---
+++ src/main/java/org/fatec/les/controller/managedbean/PedidoMB.java
@@ -0,0 +1,38 @@
+package org.fatec.les.controller.managedbean;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import org.fatec.les.controller.util.MsgUtil;
+import org.fatec.les.model.entity.PedidoEntity;
+import org.fatec.les.repositorio.PedidoRepositorio;
+
+@ManagedBean
+@ViewScoped
+public class PedidoMB implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private PedidoEntity pedido;
+	
+	public PedidoMB() {
+		pedido = new PedidoEntity();
+	}
+	
+	public PedidoEntity getPedidoEntity() {
+		return this.pedido;
+	}
+	
+	public void setPedidoEntity(PedidoEntity pedido) {
+		this.pedido = pedido;
+	}
+	
+	public void actionCadastrarPedido() {
+		PedidoRepositorio repositorio = new PedidoRepositorio();
+		repositorio.persist(pedido);
+		MsgUtil.addInfo("Pedido inserido com sucesso");
+	}
+	
+}
